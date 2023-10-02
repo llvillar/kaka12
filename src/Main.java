522f116723b6fae2b317c020571d3717c99a280d
llvillar-iesoretania <llopez@iesoretania.es>
2023-10-02 10:50:07 +0200

    cosas4
    Signed-off-by: llvillar-iesoretania <llopez@iesoretania.es>

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,6 @@ public class Main {
         System.out.printf("Hello and welcome!");
 
         for (int i = 1; i <= 5; i++) {
-
             // for you, but you can always add more by pressing Ctrl+F8.
             System.out.println("i = " + i);
         }
